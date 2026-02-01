13b2d22f8b6053d44f7c9eff98ad2199ab3316ef
Daniel Vieites <dvieitest@gmail.com>
2026-02-01 21:49:42 +0100

    chore(frontend): add eslint configuration

---
+++ apps/frontend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/jsx-uses-vars": "error",
+  },
+};
